1a570b18c205cc6da93091b6967cfef5234e8ad7
PiringerMark <Piringerm@kkszki.hu>
2026-09-18 12:20:31 +0200

    aaaaaaaaaaaaaaaaaaaaaa

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@
         {
             csomagok cs1 = new csomagok("ABC U.7",10.5,2.3,"Nagy Béla","Kis Miska","T,TWU");
             Console.WriteLine(cs1);
-            postairoda PI1 = new postairoda(0,"N.Posta Iroda");
+            postairoda PI1 = new postairoda("N.Posta Iroda");
             Console.WriteLine(PI1);
             PI1.csomag_feladas(cs1);
             Console.WriteLine(PI1);
--- postairoda.cs
+++ postairoda.cs
@@ -9,29 +9,22 @@ namespace sajatoasszekotott
 {
     internal class postairoda
     {
-        private int csomag_szam;
         private string postairoda_nev;
         private List<csomagok> csomag;
-        public postairoda(int csomag_szam, string postairoda_nev, List<csomagok> csomag)
+        public postairoda(string postairoda_nev, List<csomagok> csomag)
         { 
             this.csomag = csomag;
-            this.csomag_szam = csomag_szam;
             this.postairoda_nev = postairoda_nev;
         }
-        public postairoda(int csomag_szam, string postairoda_nev)
+        public postairoda( string postairoda_nev)
         {
             this.csomag = new List<csomagok>();
-            this.csomag_szam = csomag_szam;
             this.postairoda_nev = postairoda_nev;
         }
         public bool csomag_feladas(csomagok Csomag)
-        {
-            if (csomag.Count < csomag_szam)
-            {
+        {   
                 csomag.Add(Csomag);
                 return true;
-            }
-            return false;
         }
         public bool csomagatvetel(csomagok Csomag) 
         {
